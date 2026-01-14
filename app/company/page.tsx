1455ed1d2986cee73d7d64fe2bfcfddddf16da61
Piyush289kumar <piyushraikwar289@gmail.com>
2026-01-14 14:47:12 +0530

    Remove component.

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -1,12 +1,9 @@
 import { BackgroundRippleEffect } from "@/components/aceternity-ui/BackgroundRippleEffec";
-import { FeaturesSection } from "@/components/aceternity-ui/sections/FeatureSection";
 
 export default function CompanyPage() {
   return (
     <>
       <BackgroundRippleEffect />
-      <FeaturesSection />
-
     </>
   );
 }
