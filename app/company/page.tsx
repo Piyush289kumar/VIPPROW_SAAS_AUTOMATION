a3b59b89fb96e6c6484bfafe4724e52db7512c58
Piyush289kumar <piyushraikwar289@gmail.com>
2026-01-15 11:54:33 +0530

    Add Partner Brand Marquee.

--- app/company/page.tsx
+++ app/company/page.tsx
@@ -1,9 +1,11 @@
 import { BackgroundRippleEffect } from "@/components/aceternity-ui/BackgroundRippleEffec";
+import { PartnerBrandMarquee } from "@/components/magic-ui/PartnerBrandMarquee";
 
 export default function CompanyPage() {
   return (
     <>
       <BackgroundRippleEffect />
+      <PartnerBrandMarquee />
     </>
   );
 }
