e91a55fda9995b3a744c797892bb85c46f14374f
Piyush289kumar <piyushraikwar289@gmail.com>
2026-02-02 15:57:28 +0530

    Digital Marketing Services Page Done.

--- website/app/features/testimonial/services/testimonial.api.ts
+++ website/app/features/testimonial/services/testimonial.api.ts
@@ -4,6 +4,6 @@ import { TestimonialApiResponse, TestimonialQuery } from "@/types/testimonial";
 export const fetchTestimonials = async (
   params: TestimonialQuery = {},
 ): Promise<TestimonialApiResponse> => {
-  const res = await API.get("/testimonial", { params });
+  const res = await API.get("/testimonial/public", { params });
   return res.data;
 };
--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <body
-        className={`${inter.variable} ${poppins.variable} antialiased bg-blacks`}
+        className={`${inter.variable} ${poppins.variable} antialiased bg-black`}
       >
         <ReduxProvider>
           <QueryProvider>
