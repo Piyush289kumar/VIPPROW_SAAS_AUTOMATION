2885b4462e11103990c60636defa839f81bd5948
Piyush289kumar <piyushraikwar289@gmail.com>
2026-02-19 15:16:47 +0530

    Fix formatting in axiosClient.ts and add newline at end of file

--- website/lib/axiosClient.ts
+++ website/lib/axiosClient.ts
@@ -1,5 +1,6 @@
-import axios from 'axios'
+import axios from "axios";
 export const API = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
-    withCredentials: true
-})
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+// withCredentials: true
