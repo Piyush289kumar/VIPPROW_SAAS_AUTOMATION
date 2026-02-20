4be5ffa1a5cbe5d16096cc3a53c5282223a6be12
kumkumnema07 <kumkumnema7@gmail.com>
2026-02-20 13:14:57 +0530

    digital marketing,Business Automation,

--- website/app/digital-marketing/page.tsx
+++ website/app/digital-marketing/page.tsx
@@ -3,6 +3,10 @@ import ClientTestimonialMarquee from "@/components/client-sections/ClientTestimo
 import DigitalMarketingHeroSection from "@/components/custom-ui/DigitalMarketingHeroSection";
 import InfoSection from "@/components/custom-ui/InfoSection";
 import DigitalMarketingFeatureSection from "@/components/mvpblock-ui/DigitalMarketingFeatureSection";
+import DigitalDes from "@/components/ui/cards/DigitalDes";
+
+import DigitalHighlighted from "@/components/ui/cards/DigitalHighlited";
+
 import ServiceGridScroller from "@/components/ui/cards/ServiceGridScroller";
 import PrimaryHeading from "@/components/ui/heading/PrimaryHeading";
 
@@ -17,8 +21,6 @@ export default function DigitalMarketingPage() {
       <InfoSection />
       {/* Info Section End */}
 
-
-      
       {/* Digital Marketing Feature Start */}
       <div className="pt-20 max-w-7xl mx-auto">
         <PrimaryHeading
@@ -29,26 +31,30 @@ export default function DigitalMarketingPage() {
       </div>
       {/* Digital Marketing Feature End */}
 
-
       {/* Services Start */}
-      <ServiceGridScroller
-        initialTab="Marketing"
-        showTabs={false}
-      />
+      <ServiceGridScroller initialTab="Marketing" showTabs={false} />
 
       {/* <ServiceGridScroller /> */}
       {/* Services End */}
-
+      {/* digital marketing me koi speciality batani ho to 1*/}
+      <div className="pt-20 max-w-7xl mx-auto">
+        <PrimaryHeading
+          heading="Vipprow Digital Marketing Services  "
+          des="Authentic stories from brands that achieved measurable growth through Vipprow’s strategic approach."
+        />
+        <DigitalDes />
+      </div>
+      {/* digital marketing me koi speciality batani ho to 2*/}
+      
       {/* Testimonial Start */}
       <div className="pt-20 max-w-7xl mx-auto">
         <PrimaryHeading
-          heading="Empower Your Workflow with AI"
-          des="Ask your AI Agent for real-time collaboration, seamless integrations, and actionable insghts to streamline your operations."
+          heading="Client Experiences That Matter"
+          des="Authentic stories from brands that achieved measurable growth through Vipprow’s strategic approach."
         />
         <ClientTestimonialMarquee />
       </div>
       {/* Testimonial End */}
-
 
       {/* CTA Start */}
       <div className="pt-20 max-w-7xl mx-auto">
